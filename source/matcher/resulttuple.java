5f6a1547ce21712599ddadb13f78967adee4bd5d
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-16 22:28:59 +0300

    Create resulttuple.java

---
+++ source/matcher/resulttuple.java
@@ -0,0 +1,57 @@
+/*
+ * A ResultTuple corresponds to one Match but adds additional
+ * information about the vulnerability (such as score, reference, ...) 
+ */
+package matcher;
+
+import java.util.ArrayList;
+
+public class ResultTuple {
+	private String summary, score, accessComplexity;
+	private ArrayList<String> reference, hostname;
+
+	public ResultTuple() {
+		this.reference = new ArrayList<String>();
+		this.hostname = new ArrayList<String>();
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname.add(hostname);
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+	public void setAccessComplexity(String accessComplexity) {
+		this.accessComplexity = accessComplexity;
+	}
+
+	public void setReference(String reference) {
+		this.reference.add(reference);
+	}
+
+	public ArrayList<String> getHostname() {
+		return hostname;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public String getAccessComplexity() {
+		return accessComplexity;
+	}
+
+	public ArrayList<String> getReference() {
+		return reference;
+	}
+}
